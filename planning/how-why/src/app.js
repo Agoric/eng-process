b32b85428318947b506b1470307bc691eb778569
Dan Connolly <connolly@agoric.com>
2022-08-07 16:25:50 -0500

    feat(how-why): visualize transitive dependencies
      - dedup issues by URL
        - get repo info for dependencies
        - static types for IssueKey, IssueInfo etc.
      - render dot for a Map<url, IssueInfo>
      - condition form details on available info

--- planning/how-why/src/app.js
+++ planning/how-why/src/app.js
@@ -40,6 +40,28 @@ const Agoric = {
   workspaceId: "Z2lkOi8vcmFwdG9yL1plbmh1YlVzZXIvNjM0NDE4",
 };
 
+/**
+ * @typedef {{
+ *   repository: {
+ *     ownerName: string,
+ *     name: string,
+ *     ghId: number,
+ *   },
+ *   number: number,
+ * }} IssueKey
+ * @typedef {IssueKey & {title: string}} IssueLeaf
+ *
+ * @typedef {{
+ *  state: 'OPEN' | 'CLOSED',
+ *  number: number,
+ *  title: string,
+ *  repository: { ghId: number, ownerName: string, name: string },
+ *  blockedIssues: { nodes: IssueLeaf[] },
+ *  blockingIssues: { nodes: IssueLeaf[] },
+ *  body: string,
+ *  labels: { nodes: { name: string, color: string}[] },
+ * }} IssueInfo
+ */
 const issueQuery = `
 query getIssueInfo($repositoryGhId: Int!, $issueNumber: Int!) {
   issueByInfo(repositoryGhId: $repositoryGhId, issueNumber: $issueNumber) {
@@ -55,12 +77,22 @@ query getIssueInfo($repositoryGhId: Int!, $issueNumber: Int!) {
       nodes {
         number
         title
+        repository {
+          ghId
+          ownerName
+          name
+        }
       }
     }
     blockingIssues {
       nodes {
         number
         title
+        repository {
+          ghId
+          ownerName
+          name
+        }
       }
     }
     body
@@ -94,38 +126,45 @@ const reposQuery = `
 
 const Error = (error) => html`<p><strong>${error.message}</strong></p> `;
 
-const issueDepDot = (result) => {
-  const {
-    data: { issueByInfo },
-  } = result;
-  const {
-    repository: { ownerName, name },
-    number,
-    state,
-    title,
-    labels: { nodes: labels },
-    body,
-    blockedIssues: { nodes: blockedIssues },
-    blockingIssues: { nodes: blockingIssues },
-  } = issueByInfo;
-
-  const issues = [issueByInfo, ...blockedIssues, ...blockingIssues];
-  const deps = [
-    ...blockedIssues.map((hd) => [number, hd.number]),
-    ...blockingIssues.map((tl) => [tl.number, number]),
-  ];
+/** @param {Map<string, IssueInfo>} issueDetail */
+const issueDepDot = (issueDetail) => {
+  /** @param {IssueLeaf} k */
   const node = ({ number, title }) =>
     ` issue${number} [label="#${number}", tooltip=${JSON.stringify(title)}]`;
-  const edge = ([tl, hd]) => `  issue${tl} -> issue${hd}`;
+
+  /** @type {(e: {tail: number, head: number}) => string} */
+  const edge = ({ tail, head }) => `  issue${tail} -> issue${head}`;
+
+  /** @param {IssueInfo} issueByInfo */
+  const lines = (issueByInfo) => {
+    const {
+      repository: { ownerName, name },
+      number,
+      blockedIssues: { nodes: blockedIssues },
+      blockingIssues: { nodes: blockingIssues },
+    } = issueByInfo;
+
+    const issues = [issueByInfo, ...blockedIssues, ...blockingIssues];
+    const deps = [
+      ...blockedIssues.map((hd) => ({ tail: number, head: hd.number })),
+      ...blockingIssues.map((tl) => ({ tail: tl.number, head: number })),
+    ];
+    return [...issues.map(node), ...deps.map(edge)];
+  };
+  const dedup = (xs) => [...new Set(xs)];
   return `
   digraph {
-    ${issues.map(node).join("\n")}
-    ${deps.map(edge).join("\n")}
+    ${dedup([...issueDetail.values()].flatMap(lines)).join("\n")}
   }`;
 };
 
+/** @param {{ ownerName: string, name: string }} k */
 const repoUrl = ({ ownerName, name }) =>
   `https://github.com/${ownerName}/${name}`;
+
+/** @param {{ repository: { ownerName: string, name: string }, number: number }} k */
+const issueUrl = ({ repository: { ownerName, name }, number }) =>
+  `https://github.com/${ownerName}/${name}/issues/${number}`;
 
 const repositorySelection = (repositories, issueKey) => {
   if (!repositories.data) {
@@ -160,53 +199,51 @@ const repositorySelection = (repositories, issueKey) => {
   </select>`;
 };
 
+/**
+ * @param {IssueKey} issueKey
+ * @param {(v: IssueKey) => void} setIssueKey
+ * @param {Map<string, IssueInfo>} issueDetail
+ * @param {unknown} repositories
+ */
 const Issue = (issueKey, setIssueKey, issueDetail, repositories) => () => {
-  if (!issueDetail.data) {
-    return html`<em>no issueDetail available yet</em>`;
-  }
+  const issueByInfo = issueDetail.get(issueUrl(issueKey));
 
   const onSubmit = (ev) => {
     const issueNumber = parseInt(ev.target.querySelector("input").value, 10);
-    setIssueKey({ ...issueKey, issueNumber });
+    setIssueKey({ ...issueKey, number: issueNumber });
     ev.preventDefault();
   };
 
   const repoControl = repositorySelection(repositories, issueKey);
 
   const numControl = html`<label
-    >#<input size="5" type="number" value="${issueKey.issueNumber}"
+    >#<input size="5" type="number" value="${issueKey.number}"
   /></label>`;
 
-  const {
-    data: {
-      issueByInfo: {
-        repository: { ownerName, name },
-        number,
-        state,
-        title,
-        labels: { nodes: labels },
-        body,
-        blockedIssues: { nodes: blockedIssues },
-        blockingIssues: { nodes: blockingIssues },
-      },
-    },
-  } = issueDetail;
-  const url = `https://github.com/${ownerName}/${name}/issues/${number}`;
-
-  return html`<h2>Issue Dependencies</h2>
+  const form = (detail) => html`<h2>Issue Dependencies</h2>
     <form onSubmit=${onSubmit}>
-      <fieldset>
-        ${repoControl} ${numControl}${" "}
-        <a href="${url}">#${number} ${title}</a>: ${state}
-        <p>
-          Labels:
-          ${labels.map(
-            ({ name, color }) => html`<b class="label">${name}</b> `
-          )}
-        </p>
-      </fieldset>
+      <fieldset>${repoControl} ${numControl}${" "} ${detail}</fieldset>
     </form>`;
+
+  if (!issueByInfo) {
+    return form(html`<a href="${issueUrl(issueKey)}">#${issueKey.number}</a>`);
+  }
+
+  const {
+    number,
+    state,
+    title,
+    labels: { nodes: labels },
+  } = issueByInfo;
+
   //  <small>${body}</small>
+  return form(html`
+    <a href="${issueUrl(issueKey)}">#${number} ${title}</a>: ${state}
+    <p>
+      Labels:
+      ${labels.map(({ name, color }) => html`<b class="label">${name}</b> `)}
+    </p>
+  `);
 };
 
 const ZenHub = {
@@ -223,11 +260,19 @@ const App =
     const [apiKey, setKey] = useState(localStorage.getItem(key));
     const [reason, setReason] = useState(null);
     const [repositories, setRepositories] = useState({});
+
+    /** @template T @typedef {[T, import("preact/hooks").StateUpdater<T>]} StateT<T> */
+    /** @type {StateT<IssueKey>} */
     const [issueKey, setIssueKey] = useState({
-      repositoryGhId: Agoric.repositories["agoric-sdk"],
-      issueNumber: 1,
+      repository: {
+        ghId: Agoric.repositories["agoric-sdk"],
+        ownerName: "Agoric",
+        name: "agoric-sdk",
+      },
+      number: 1,
     });
-    const [issueDetail, setIssueDetail] = useState({});
+    /** @type {StateT<Map<string, IssueInfo>>} */
+    const [issueDetail, setIssueDetail] = useState(new Map());
 
     useEffect(() => {
       const endpoint = graphql(ZenHub.endpoint, apiKey, { fetch });
@@ -255,26 +300,50 @@ const App =
       const runQuery = async () => {
         setReason(null);
 
-        endpoint
-          .query(issueQuery, {
-            workspaceId: Agoric.workspaceId,
-            ...issueKey,
-          })
-          .then((result) => {
-            console.log("issue query result:", result);
-            setIssueDetail(result);
-            const dot = issueDepDot(result);
-            console.log(dot);
-            globalThis.renderDot(dot);
-          })
-          .catch((r) => {
-            setReason(r);
-          });
+        const done = [];
+        /** @type {Set<IssueKey>} */
+        const todo = new Set([issueKey]);
+        /** @type {typeof issueDetail} */
+        const updated = new Map();
+
+        for await (const current of todo) {
+          console.debug("transitive deps:", { current, done });
+          if (done.includes(issueUrl(current))) continue;
+          await endpoint
+            .query(issueQuery, {
+              workspaceId: Agoric.workspaceId,
+              repositoryGhId: current.repository.ghId,
+              issueNumber: current.number,
+            })
+            .then((result) => {
+              console.log("issue query result:", result);
+              if ("data" in result) {
+                /** @type {{data: {issueByInfo: IssueInfo}}} */
+                const {
+                  data: { issueByInfo },
+                } = result;
+                done.push(issueUrl(issueByInfo));
+                updated.set(issueUrl(issueByInfo), issueByInfo);
+                // issueByInfo.blockedIssues.nodes.forEach((i) => todo.add(i));
+                issueByInfo.blockingIssues.nodes.forEach((i) => todo.add(i));
+              } else {
+                console.warn("TODO: handle query failures?", result);
+              }
+            })
+            .catch((r) => {
+              setReason(r);
+              throw r;
+            });
+        }
+        setIssueDetail(updated);
+        const dot = issueDepDot(updated);
+        console.log(dot);
+        globalThis.renderDot(dot);
       };
       runQuery();
     }, [apiKey, issueKey]);
 
-    console.log("App state", { reason, issueKey, repositories });
+    console.log("App state", { reason, issueKey, issueDetail, repositories });
     return html`
       <div>
         <${APIKey(apiKey, setKey)} />
